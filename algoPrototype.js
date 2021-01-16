0b4b6c13ae3a978c30ef63e82215e54f47356a9c
puthpiseth <puthpiseth_tun@yahoo.com>
2021-01-16 20:21:10 +0100

    Third Commit

--- algoPrototype.js
+++ algoPrototype.js
@@ -1,77 +1,118 @@
-function MoveInSpace(x = 4, y = 5, player = { x: 0, y: 0, direction: "S" }) {
-    this.x = x;
-    this.y = y;
-    this.player = player;
+// "use strict";
+
+// let x = 4;
+// let y = 5;
+
+// var toto = 'toto';
+
+
+// let  direction="S";
+
+// function turn(instruction) {
+//     if (instruction === "gauche") {
+//         switch (direction) {
+//             case "S":
+//                 direction = "E";
+//                 break;
+
+//             case "E":
+//                 direction = "N";
+//                 break;
+
+//             case "O":
+//                 direction = "S"
+//                 break;
+
+//             case "N":
+//                 direction = "O"
+//                 break;
+
+//         }
+//     }
+//     else if (instruction === "droite") {
+//         switch (direction) {
+//             case "S":
+//                 direction = "O";
+//                 break;
+
+//             case "E":
+//                 direction = "S";
+//                 break;
+
+//             case "O":
+//                 direction = "N"
+//                 break;
+
+//             case "N":
+//                 direction = "E"
+//                 break;
+
+//             default:
+//                 break;
+//         }
+//     }
+//     console.log(direction);
+// }
+
+// turn("gauche"); // Est
+// turn("gauche"); // Nord
+// turn("droite"); // Est
+
+
+function MoveInSpace(x,y,direction){
+    this.x=x;
+    this.y=y;
+    this.direction=direction;
 }
 
-MoveInSpace.prototype.displayGrid = function() {
-    let str = "";
-    
-    for(let a = 0; a < this.y; a++) {
-        for (let a = 0; a < this.x; a++) {
-            str += "+-+";
-        }
-        str += "\n";
-        for (let a = 0; a < this.x; a++) {
-            str += "| |"; 
-        }
-        str += "\n";
-    }
-    for (let a = 0; a < this.x; a++) {
-        str += "+-+";
-    }
-
-    console.log(str);
+MoveInSpace.prototype.turn=function(instruction){
+    if (instruction === "gauche") {
+                switch (this.direction) {
+                    case "S":
+                        this.direction = "E";
+                        break;
+        
+                    case "E":
+                        this.direction = "N";
+                        break;
+        
+                    case "O":
+                        this.direction = "S"
+                        break;
+        
+                    case "N":
+                        this.direction = "O"
+                        break;
+        
+                }
+            }
+            else if (instruction === "droite") {
+                switch (this.direction) {
+                    case "S":
+                        this.direction = "O";
+                        break;
+        
+                    case "E":
+                        this.direction = "S";
+                        break;
+        
+                    case "O":
+                        this.direction = "N"
+                        break;
+        
+                    case "N":
+                        this.direction = "E"
+                        break;
+        
+                    default:
+                        break;
+                }
+            }
+            console.log(this.direction);
 
 }
 
-MoveInSpace.prototype.turn = function (instruction) {
-        if (instruction === "gauche") {
-        switch (this.direction) {
-            case "S":
-                this.direction = "E";
-                break;
 
-            case "E":
-                this.direction = "N";
-                break;
-
-            case "O":
-                this.direction = "S"
-                break;
-
-            case "N":
-                this.direction = "O"
-                break;
-
-        }
-    }
-    else if (instruction === "droite") {
-        switch (this.direction) {
-            case "S":
-                this.direction = "O";
-                break;
-
-            case "E":
-                this.direction = "S";
-                break;
-
-            case "O":
-                this.direction = "N"
-                break;
-
-            case "N":
-                this.direction = "E"
-                break;
-
-            default:
-                break;
-        }
-    }
-    console.log(this.direction)
-}
-
-let space = new MoveInSpace();
-space.turn('gauche');
-space.turn('gauche');
-space.displayGrid();
+let space=new MoveInSpace(4,5,"S");
+space.turn("gauche");
+space.turn("gauche");
